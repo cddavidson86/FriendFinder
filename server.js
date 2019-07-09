fa28f93f886af44e22b9467b7ee5ce9b289d507a
Charles Davidson <cddavidson86@yahoo.com>
2019-07-08 20:57:33 -0500

    created modal

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -10,7 +9,6 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.listen(PORT, () => console.log("Listening on port 3000"));
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
@@ -18,12 +16,3 @@ require("./app/routing/htmlRoutes.js")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-
-// // Basic route that sends the user first to the AJAX Page
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/home.html"));
-// });
-
-// app.get("/survey", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/survey.html"));
-// });
