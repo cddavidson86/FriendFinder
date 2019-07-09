a6675833efeb7134900b07f6e6b97a30322da14b
Charles Davidson <cddavidson86@yahoo.com>
2019-07-09 14:41:13 -0500

    fixed picture size and updated readme

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,12 +3,12 @@ var friends = [ {
     "photo" : "https://static.fustany.com/images/en/photo/small_haircut_and_hairstyles_trends_in_spring_summer_2019_fustany_image_8.jpg",
     "scores" : [5,4,2,5,1,1,2,1,3,5]
 }, {
-    "name" : "Sherri",
-    "photo" : "https://static.fustany.com/images/en/photo/small_haircut_and_hairstyles_trends_in_spring_summer_2019_fustany_image_8.jpg",
+    "name" : "Frank",
+    "photo" : "http://www.citycabllc.com/wp-content/uploads/parser/good-haircuts-for-thick-hair-guys-1.jpg",
     "scores" : [3,4,2,4,4,1,1,2,3,4]
 }, {
     "name" : "Jessie",
-    "photo" : "https://static.fustany.com/images/en/photo/small_haircut_and_hairstyles_trends_in_spring_summer_2019_fustany_image_8.jpg",
+    "photo" : "https://www.menshairstyletrends.com/wp-content/uploads/2016/11/ppreshaw-slick-side-part-hairstyle-for-men.jpg",
     "scores" : [2,3,5,5,3,5,2,1,3,2]
 }];
 
